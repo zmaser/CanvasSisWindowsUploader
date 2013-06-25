c12fe180eac3fcd3ecf40c0024f7da169d56a56e
Zack Maser <zmaser@gmail.com>
2013-06-25 11:45:15 -0300

    First Commit
    The code works... on my machine.

---
+++ CanvasDeploy.ps1
@@ -0,0 +1,49 @@
+# location of the canvas csv files
+$csvFiles = "C:\CanvasScripts\canvas\*.*"
+
+# location to put the resulting zip file
+$zipFile = "C:\CanvasScripts\canvas.zip"
+
+# Replace <canvas site url> with your site ex. college.instructure.com
+$apiUrl = "https://<canvas site url>/api/v1/accounts/1/sis_imports.json?import_type=instructure_csv&extension=zip"
+$authToken = "1527~ouRYhqHx2ycjlx73GFS342341tA3UILdNRIDOSegvSYI5tEbqTmZLpRe1IAPZg7B"
+
+
+dir $csvFiles | Remove-UTF8Bom
+dir $csvFiles | Add-Zip $zipFile
+
+$zipByteArray = [io.file]::ReadAllBytes($zipFile)
+
+$webClient = New-Object System.Net.WebClient
+$webClient.Headers.Add("Authorization","Bearer $authToken")
+$webClient.UploadData($apiUrl,$zipByteArray)
+
+function Add-Zip
+{
+  param([string]$zipfilename)
+
+	if(-not (test-path($zipfilename)))
+	{
+		set-content $zipfilename ("PK" + [char]5 + [char]6 + ("$([char]0)" * 18))
+		(dir $zipfilename).IsReadOnly = $false	
+	}
+	
+	$shellApplication = new-object -com shell.application
+	$zipPackage = $shellApplication.NameSpace($zipfilename)
+	
+	foreach($file in $input) 
+	{ 
+            $zipPackage.CopyHere($file.FullName)
+            Start-sleep -milliseconds 2000
+	}
+}
+
+function Remove-UTF8Bom
+{
+	foreach($file in $input) 
+	{ 
+	    (Get-Content $file) -replace [char]0x00EF + [char]0x00BB + [char]0x00BF  | Set-Content $file
+	}
+}
+
+
